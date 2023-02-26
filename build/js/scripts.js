f1385686b642fc4fab88f07c58eee7c32350c98e
Yakup <yakupjoraev@gmail.com>
2023-02-26 11:20:39 +0300

    fixind

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,5 +1,20 @@
 // Custom Scripts
 // Custom scripts
+//плавный скролл
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+
+    const blockID = anchor.getAttribute('href').substr(1)
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+};
 
 // Мобильное меню бургер
 function burgerMenu() {
--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,19 @@
 // Custom scripts
+//плавный скролл
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+
+    const blockID = anchor.getAttribute('href').substr(1)
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+};
 
 // Мобильное меню бургер
 function burgerMenu() {
